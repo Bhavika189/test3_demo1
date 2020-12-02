88a20fdcd75d40dab6f523d1e82c583a5e740f9a
shaisthajabeen610 <67311615+shaisthajabeen610@users.noreply.github.com>
2020-12-02 09:43:41 +0530

    Update sample1.java
    changed the statement hello world to shaistha jabeen.

--- sample1.java
+++ sample1.java
@@ -1,7 +1,7 @@
 public class Solution {
 
     public static void main(String[] args) {
-        System.Out.Println("Hello, World.");
+        System.Out.Println("Shaistha jabeen");
         System.Out.Println("Hello, Java.")
         
         }
