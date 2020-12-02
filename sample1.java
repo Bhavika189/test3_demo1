eb595b371da47908b4d3f4b6147337a35115ca4b
shaisthajabeen610 <67311615+shaisthajabeen610@users.noreply.github.com>
2020-12-02 09:38:28 +0530

    just a trial

---
+++ sample1.java
@@ -0,0 +1,8 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        System.Out.Println("Hello, World.");
+        System.Out.Println("Hello, Java.")
+        
+        }
+}
